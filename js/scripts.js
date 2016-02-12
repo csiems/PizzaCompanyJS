26bb4085bd021c0cb633ae8149f78bebc47efa6f
Christopher Siems <csiems@gmail.com>
2016-02-12 08:49:24 -0800

    create pizza class

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function Pizza(name, address, zip, size) {
+  this.name = name;
+  this.address = address;
+  this.zip = zip;
+  this.size = size;
+  this.toppings = [];
+  this.premiumToppings = [];
+  this.delivery = false;
+}
+
+  // Pizza.prototype.addTopping = function (topping) {
+  // };
+  //
+  // Pizza.prototype.removeTopping = function (topping) {
+  //   // body...
+  // };
+  //
+  // Pizza.prototype.addDelivery = function (first_argument) {
+  //   // body...
+  // };
+  //
+  // Pizza.prototype.calculateCost = function (first_argument) {
+  //
+  // };
+  //
+  // function ShoppingCart() {
+  //   this.pizzas = [];
+  // }
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,12 @@
-describe('', function() {
-  it("", function() {
-    var  = new ("");
-    expect().to.equal("");
+describe('Pizza', function() {
+  it("will be created with the given properties", function() {
+    var testPizza = new Pizza("Christopher", "1618 N Killingsworth", "97217", "12-inch");
+    expect(testPizza.name).to.equal("Christopher");
+    expect(testPizza.address).to.equal("1618 N Killingsworth");
+    expect(testPizza.zip).to.equal("97217");
+    expect(testPizza.size).to.equal("12-inch");
+    expect(testPizza.toppings).to.be.empty;
+    expect(testPizza.premiumToppings).to.be.empty;
+    expect(testPizza.delivery).to.be.false;
   });
 });
